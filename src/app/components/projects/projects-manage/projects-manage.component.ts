04479fed97ecfe4bccecff74d4c232d951d11fcb
Pankaj Makwana <pankaj.m@wingstechsolutions.com>
2019-02-11 17:44:24 +0530

    update code with adding multiple users to project

--- src/app/components/projects/projects-manage/projects-manage.component.ts
+++ src/app/components/projects/projects-manage/projects-manage.component.ts
@@ -3,11 +3,13 @@ import {
   FormBuilder,
   FormGroup,
   Validators,
-  ReactiveFormsModule
+  ReactiveFormsModule,
+  FormArray
 } from "@angular/forms";
 import { HttpRequestService } from "src/app/services/http-request.service";
 import { Route } from "@angular/compiler/src/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { EMAIL_VALIDATOR } from '@angular/forms/src/directives/validators';
 
 @Component({
   selector: "app-projects-manage",
@@ -15,7 +17,8 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./projects-manage.component.css"]
 })
 export class ProjectsManageComponent implements OnInit {
-  public projectForm : FormGroup;
+  public projectForm: FormGroup;
+  public users:FormArray;
   public operationText = "Add";
 
   public statues = [
@@ -23,6 +26,11 @@ export class ProjectsManageComponent implements OnInit {
     { option: "2", value: "Status 2" },
     { option: "3", value: "Status 3" },
     { option: "4", value: "Status 4" }
+  ];
+  public userTypes = [
+    { option: 1, value: "Type 1" },
+    { option: 2, value: "Type 2" },
+    { option: 3, value: "Type 3" }
   ];
   // public project = IProject;
   public project = {
@@ -38,7 +46,7 @@ export class ProjectsManageComponent implements OnInit {
     public formBuilder: FormBuilder,
     public httpRequest: HttpRequestService,
     public route: ActivatedRoute,
-    private router: Router,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -52,14 +60,36 @@ export class ProjectsManageComponent implements OnInit {
     }
   }
   initForm() {
+    let oldUsers = this.project.users;
+    let that = this;
+    let oldUser = Array();
+    oldUsers.forEach(element => {
+      oldUser.push(this.createUser(element));
+    });
+    if(oldUsers.length === 0){
+      oldUser.push(this.createUser({}));
+    }
     this.projectForm = this.formBuilder.group({
       name: [this.project.name, Validators.required],
       description: [this.project.description, Validators.required],
       startDate: [this.project.startDate, Validators.required],
       endDate: [this.project.endDate, Validators.required],
       status: [this.project.status, Validators.required],
-      users: []
+      users: this.formBuilder.array(oldUser)
+      // users: this.formBuilder.array([this.createUser()])
     });
+  }
+  createUser(user): FormGroup {
+    return this.formBuilder.group({
+      name: [user.name, Validators.required],
+      type: [user.type, Validators.required],
+      email: [user.email, [Validators.required]],
+      // _id: [user._id, []]
+    });
+  }
+  addUser(): void {
+    this.users = this.projectForm.get('users') as FormArray;
+    this.users.push(this.createUser({}));
   }
   getProject(id) {
     this.httpRequest.doGet("projects/" + id).subscribe((res: any) => {
@@ -68,21 +98,28 @@ export class ProjectsManageComponent implements OnInit {
       this.project.startDate = res.startDate;
       this.project.endDate = res.endDate;
       this.project.status = res.status;
+      this.project.users = res.users;
       this.initForm();
     });
   }
   projectCreate() {
     let that = this;
     let id = this.route.snapshot.params["id"];
+    // console.log(this.projectForm.value);
+    // return false;
     if (!id) {
       let data = this.projectForm.value;
-      data.users = JSON.parse('[{"name":"name4","type":2,"email":"email@email.com"},{"name":"name4","type":1,"email":"email144@email.com"}]');
-      this.httpRequest.doPostWithoutHeader("projects", this.projectForm.value).subscribe(
-        (data: any) => {
-          that.router.navigate(["/projects"]);
-        },
-        (err: any) => {}
-      );
+      // data.users = JSON.parse(
+      //   '[{"name":"name4","type":2,"email":"email@email.com"},{"name":"name4","type":1,"email":"email144@email.com"}]'
+      // );
+      this.httpRequest
+        .doPostWithoutHeader("projects", this.projectForm.value)
+        .subscribe(
+          (data: any) => {
+            that.router.navigate(["/projects"]);
+          },
+          (err: any) => {}
+        );
     } else {
       this.httpRequest
         .doPutWithoutHeader("projects/" + id, this.projectForm.value)
